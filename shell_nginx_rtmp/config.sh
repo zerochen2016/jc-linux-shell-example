570c4a7e2133c8eda0b69f4693fc6811dd94652c
zerochen2016 <470133773@qq.com>
2019-12-21 23:18:45 +0800

    add shell for nginx rtmp

---
+++ shell_nginx_rtmp/config.sh
@@ -0,0 +1,5 @@
+./auto/configure --prefix=/usr/local/nginx \
+        --with-http_v2_module \
+        --with-http_flv_module \
+        --with-http_mp4_module \
+        --add-module=../nginx-rtmp-module/
