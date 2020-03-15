27397b404a92016e2a14dc7be8df320f2f6ab2fe
zerochen2016 <470133773@qq.com>
2020-03-15 13:11:42 +0800

    replace android icon

---
+++ file_example/replace_appicon_for_android.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Enter filename:"
+cp $REPLY'48.png' /Users/zero/Desktop/mywork/aworkspace/yyj-zb-android/app/src/main/res/mipmap-mdpi/ic_launcher.png
+cp $REPLY'72.png' /Users/zero/Desktop/mywork/aworkspace/yyj-zb-android/app/src/main/res/mipmap-hdpi/ic_launcher.png
+cp $REPLY'96.png' /Users/zero/Desktop/mywork/aworkspace/yyj-zb-android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
+cp $REPLY'144.png' /Users/zero/Desktop/mywork/aworkspace/yyj-zb-android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+cp $REPLY'144.png' /Users/zero/Desktop/mywork/aworkspace/yyj-zb-android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
